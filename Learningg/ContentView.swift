da451e76108a79a57ad0886cfc11575af9ec7b2f
Adwa <adwaalsh01@icloud.com>
2024-10-29 00:44:23 +0300

    Initial Commit

---
+++ Learningg/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learningg
+//
+//  Created by Adwa on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learningg/LearninggApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearninggApp.swift
+//  Learningg
+//
+//  Created by Adwa on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearninggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
